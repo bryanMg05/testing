e5a94eab191abf89dd9b43a1efc372a87b25a25e
bryanMg05 <zS21004530@estudiantes.uv.mx>
2023-10-23 16:02:41 -0600

    cola

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MediaComponent } from './media/media/media.component';
+import { LinearRegressionComponent } from './linear-regression/linear-regression.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MediaComponent
+    MediaComponent,
+    LinearRegressionComponent
   ],
   imports: [
     BrowserModule
